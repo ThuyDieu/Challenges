63fbdfcf4df0f9b514044f424d0247877cce801c
thuydieu <dieunlt@rnt.vn>
2022-03-01 15:04:10 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-reddit';
+  // title = 'angular-reddit';
+  addArticle(title: HTMLInputElement, link: HTMLInputElement):boolean{
+    console.log(`Adding Article title: ${title.value} and link ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value ='';
+    link.value = '';
+    return false;
+  }
+
+  sortedArticles():Article[]{
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes)
+  }
+
+  articles: Article[]; // <-- component property
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
 }
